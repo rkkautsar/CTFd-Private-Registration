443e44b1cfa299fda22d10d7eff6a8449399890f
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2017-07-15 13:50:43 +0700

    revert code formatting

--- private_registration.py
+++ private_registration.py
@@ -49,16 +49,11 @@ def add_to_invitation_list(csv_file):
         email = team[1]
 
         name_len = len(name) == 0
-        valid_email = re.match(
-            r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email)
-        registered_names = Teams.query.add_columns(
-            'name').filter_by(name=name).first()
-        registered_emails = Teams.query.add_columns(
-            'email').filter_by(email=email).first()
-        invited_names = InvitedTeams.query.add_columns(
-            'name').filter_by(name=name).first()
-        invited_emails = InvitedTeams.query.add_columns(
-            'email').filter_by(email=email).first()
+        valid_email = re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email)
+        registered_names = Teams.query.add_columns('name').filter_by(name=name).first()
+        registered_emails = Teams.query.add_columns('email').filter_by(email=email).first()
+        invited_names = InvitedTeams.query.add_columns('name').filter_by(name=name).first()
+        invited_emails = InvitedTeams.query.add_columns('email').filter_by(email=email).first()
 
         # To check if there are any additional errors later
         errors_len = len(errors)
@@ -86,8 +81,7 @@ def add_to_invitation_list(csv_file):
     db.session.close()
 
     if number_of_invited_teams > 0:
-        messages.append(
-            'Succesfully added %d teams to invitation list' % number_of_invited_teams)
+        messages.append('Succesfully added %d teams to invitation list' % number_of_invited_teams)
 
     return messages, errors
 
@@ -121,11 +115,9 @@ def override_register_template():
     selected_option = utils.get_config('private_registration_option')
 
     if selected_option == 'token':
-        register_template_path = '{}/templates/private-registration-token.html'.format(
-            dir_path)
+        register_template_path = '{}/templates/private-registration-token.html'.format(dir_path)
     elif selected_option == 'email':
-        register_template_path = '{}/templates/private-registration-email.html'.format(
-            dir_path)
+        register_template_path = '{}/templates/private-registration-email.html'.format(dir_path)
 
     if register_template_path:
         with open(register_template_path, 'r') as register_template:
@@ -144,8 +136,7 @@ def load(app):
         for team in teams:
             registered_teams[team.name] = True
 
-        invited_teams = InvitedTeams.query.order_by(
-            InvitedTeams.id.asc()).all()
+        invited_teams = InvitedTeams.query.order_by(InvitedTeams.id.asc()).all()
 
         json = {'invited_teams': []}
         for team in invited_teams:
@@ -157,6 +148,7 @@ def load(app):
                                           'token': team.token, 'registered': registered})
 
         return jsonify(json)
+
 
     @app.route('/admin/invited_teams/delete/<int:id>', methods=['POST'])
     @admins_only
@@ -170,6 +162,7 @@ def load(app):
         else:
             return '1'
 
+
     @app.route('/admin/invited_teams/delete/all', methods=['POST'])
     @admins_only
     def remove_all_invited_teams():
@@ -182,6 +175,7 @@ def load(app):
         else:
             return '1'
 
+
     @app.route('/admin/invited_teams/export', methods=['GET'])
     @admins_only
     def export_csv():
@@ -189,6 +183,7 @@ def load(app):
         ctf_name = utils.ctf_name()
         full_name = '{}-invited-teams.csv'.format(ctf_name)
         return send_file(output, as_attachment=True, attachment_filename=full_name)
+
 
     @app.route('/admin/invited_teams/import', methods=['POST'])
     @admins_only
@@ -200,6 +195,7 @@ def load(app):
             return jsonify(json)
         else:
             return '0'
+
 
     @app.route('/admin/invited_teams/option', methods=['POST'])
     @admins_only
@@ -214,6 +210,7 @@ def load(app):
         else:
             return '0'
 
+
     @app.route('/admin/invited_teams/send_invitation', methods=['POST'])
     @admins_only
     def send_invitation_all():
@@ -225,8 +222,7 @@ def load(app):
             for team in teams:
                 registered_teams[team.name] = True
 
-            invited_teams = InvitedTeams.query.order_by(
-                InvitedTeams.id.asc()).all()
+            invited_teams = InvitedTeams.query.order_by(InvitedTeams.id.asc()).all()
 
             for team in invited_teams:
                 if team.name in registered_teams:
@@ -248,13 +244,13 @@ def load(app):
         else:
             return "0"
 
+
     @app.route('/admin/invited_teams/send_invitation/<int:id>', methods=['POST'])
     @admins_only
     def send_invitation(id):
         if utils.can_send_mail():
             selected_option = utils.get_config('private_registration_option')
-            team = InvitedTeams.query.add_columns(
-                'name', 'email', 'token').filter_by(id=id).first()
+            team = InvitedTeams.query.add_columns('name', 'email', 'token').filter_by(id=id).first()
 
             if selected_option == 'token':
                 token_text = ' with token {}'.format(team.token)
@@ -272,6 +268,7 @@ def load(app):
             return '1'
         else:
             return '0'
+
 
     def private_register():
         if not utils.can_register():
@@ -297,8 +294,7 @@ def load(app):
                 errors.append('Something strange happened')
 
             if len(errors) == 0:
-                team = Teams.query.add_columns('id').filter_by(
-                    name=invited_team.name).first()
+                team = Teams.query.add_columns('id').filter_by(name=invited_team.name).first()
                 if team:
                     errors.append('Already registered')
 
@@ -315,12 +311,12 @@ def load(app):
             if len(errors) > 0:
                 if selected_option == 'token':
                     return render_template('register.html', errors=errors,
-                                           token=request.form['token'],
-                                           password=request.form['password'])
+                                                            token=request.form['token'],
+                                                            password=request.form['password'])
                 elif selected_option == 'email':
                     return render_template('register.html', errors=errors,
-                                           email=request.form['email'],
-                                           password=request.form['password'])
+                                                            email=request.form['email'],
+                                                            password=request.form['password'])
                 else:
                     return render_template('register.html')
             else:
@@ -341,8 +337,7 @@ def load(app):
                         db.session.close()
                         logger = logging.getLogger('regs')
                         logger.warn("[{0}] {1} registered (UNCONFIRMED) with {2}".format(time.strftime("%m/%d/%Y %X"),
-                                                                                         name.encode(
-                                                                                             'utf-8'),
+                                                                                         name.encode('utf-8'),
                                                                                          email.encode('utf-8')))
 
                         utils.verify_email(team.email)
@@ -350,22 +345,22 @@ def load(app):
                         return redirect(url_for('auth.confirm_user'))
                     else:
                         if utils.can_send_mail():
-                            utils.sendmail(email, "You've successfully registered for {}".format(
-                                utils.get_config('ctf_name')))
+                            utils.sendmail(email, "You've successfully registered for {}".format(utils.get_config('ctf_name')))
 
             db.session.close()
 
             logger = logging.getLogger('regs')
             logger.warn("[{0}] {1} registered with {2}".format(time.strftime("%m/%d/%Y %X"),
-                                                               name.encode(
-                                                                   'utf-8'),
+                                                               name.encode('utf-8'),
                                                                email.encode('utf-8')))
             return redirect(url_for('challenges.challenges_view'))
         else:
             return render_template('register.html')
 
+
     if not utils.get_config('private_registration_option'):
         utils.set_config('private_registration_option', 'token')
+
 
     override_register_template()
     app.view_functions['auth.register'] = private_register
